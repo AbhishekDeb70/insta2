a693ae1cfb253b15b6439e60988c3114952c2e70
AbhishekDeb70 <32860624+AbhishekDeb70@users.noreply.github.com>
2021-08-25 23:10:43 +0530

    Update ImageUpload.js

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -11,40 +11,39 @@ function ImageUpload({lang, username, closemodal, viewwhichuser, viewsinglepost}
     const [caption, setCaption] = useState('');
 
     const handleChange = (e) => {
-        // this will pick the FIRST file selected (to avoid selecting many)
+        
         if (e.target.files[0]) {
             setImage(e.target.files[0]);
         }
     }
 
     const handleUpload = () => {
-        // This is what uploads the image to Firebase
-        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
         
         uploadTask.on(
             "state_changed",
             (snapshot) => {
-                // progress function
+               
                 const progress = Math.round(
                     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
                 );
                 setProgress(progress)
             },
             (error) => {
-                // Error function
+                
                 console.log(error);
                 alert(error.message);
             },
             () => {
-                // complete function ...
+               
                 storage
                     .ref("images")
                     .child(image.name)
                     .getDownloadURL()
                     .then(url => {
-                        // Post image URL inside db
+                       
                         db.collection("posts").add({
-                            // timestamp is used here to figure out the time the image was uploaded, which is gonna determine the order in which we display the posts (latest at the top)
+                            
                             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
                             caption: caption,
                             imageUrl: url,
@@ -52,13 +51,13 @@ function ImageUpload({lang, username, closemodal, viewwhichuser, viewsinglepost}
                             imagename: image.name
                         });
 
-                        // Reset everything once upload process is completed
+                        
                         setProgress(0);
                         setCaption("");
                         setImage(null);
                         closemodal(false);
 
-                        // Scroll back to top and reset other states so that it goes back to default list
+                       
                         document.body.scrollTop = 0; // For Safari
                         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
                         viewwhichuser('');
